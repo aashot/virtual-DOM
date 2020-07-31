6abf0a0fb6c933c29a1ef382c247cbe77c49db28
aashot <super.yourmail@yandex.com>
2020-07-31 22:21:37 +0300

    layout changes

--- src/components/app.js
+++ src/components/app.js
@@ -10,8 +10,19 @@ const h = (type, props, ...children) => {
 };
 
 export default (
-    <div class="container">
-        <div class="glitch" data-text="SUPER">SUPER</div>
-        <div class="glitch" data-text="GLITCH">GLITCH</div>
-    </div>
+    <nav class="dropdownmenu">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Me</a></li>
+            <li><a href="#">Articles on HTML5 & CSS3</a>
+                <ul id="submenu">
+                    <li><a href="#">Difference between SVG vs. Canvas</a></li>
+                    <li><a href="#">New features in HTML5</a></li>
+                    <li><a href="#">Creating links to sections within a webpage</a></li>
+                </ul>
+            </li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </nav>
 );
